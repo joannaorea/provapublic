2b97ee101828681acec7443781e9fff483232f76
joannaorea <joannaorea.alum@insestatut.cat>
2026-04-27 19:28:22 +0200

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println("5 + 3 = " + calc.sumar(5, 3));
+    }
+}
